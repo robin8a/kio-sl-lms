28e3d2dcb6fd812feec97d8db2a6c6917986a060
ROBIN OCHOA <robin8a@local.mac>
2020-09-05 21:06:48 -0500

    NavBar updated and video test

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,8 +10,12 @@ export class NavBar extends Component {
                 <button type="button" className="btn btn-default btn-lg">
                     <span className="glyphicon glyphicon-heart" aria-hidden="true"></span>
                 </button>
-
-                
+                <button type="button" className="btn btn-default btn-lg">
+                    <span className="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                </button>
+                <button type="button" className="btn btn-default btn-lg">
+                    <span className="glyphicon glyphicon-share" aria-hidden="true"></span>
+                </button>
             </nav>
         )
     }
